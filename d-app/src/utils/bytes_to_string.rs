853527fc7bd72b9497f18caae6e9bbc9475a83db
Maxime AUBURTIN <maxime@auburt.in>
2024-07-22 16:53:43 +0200

    Cleaned main branch

--- d-app/src/utils/bytes_to_string.rs
+++
@@ -1,4 +0,0 @@
-pub fn bytes_to_string(bytes: &[u8]) -> String {
-    let valid_bytes = bytes.iter().take_while(|&&b| b != 0).copied().collect::<Vec<u8>>();
-    String::from_utf8_lossy(&valid_bytes).to_string()
-}
--- d-app/src/utils/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod bytes_to_string;
-
-pub use bytes_to_string::bytes_to_string;
