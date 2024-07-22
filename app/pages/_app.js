44e77210505c55008362382a87af90f6e7e5b61c
Sebastien <sebastiengazeau4@gmail.com>
2024-07-22 22:54:34 +0200

    add env

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -3,12 +3,14 @@ import {
     WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
     PhantomWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import { useMemo } from "react";
 import { AppProvider } from "../context/context";
 import "../styles/globals.css";
+import { clusterApiUrl } from "@solana/web3.js";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 
@@ -16,9 +18,11 @@ import { useRouter } from 'next/router';
 import Header from "../components/Header";
 import style from "../styles/Home.module.css";
 function MyApp({ Component, pageProps }) {
-  // const endpoint = clusterApiUrl(WalletAdapterNetwork.Devnet); 
+
+  const endpoint = process.env.NEXT_PUBLIC_TEST_NETWORK === 'true' ? "http://127.0.0.1:8899" : clusterApiUrl(WalletAdapterNetwork.Devnet); 
   const router = useRouter();
-  const endpoint = "http://127.0.0.1:8899"; 
+
+  // const endpoint = "http://127.0.0.1:8899"; 
 
   const wallets = useMemo(
     () => [
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,5 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const PROGRAM_ID = new PublicKey(
-  "4AEtDMG3A5rFTFwj6KyA6K41dxxpagemC4CaG5w9oABc"//"8KcDj4FdPm6Dq6KoLFCAgPrAYEMbXtE9Ruc7B62UGeyJ"
+  process.env.NEXT_PUBLIC_PROGRAM_ADDRESS
 );
